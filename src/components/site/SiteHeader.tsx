c1d98a9057750f2f333a7f6545c3d1927eae4d8d
KADIR0612 <shkhakadir@gmail.com>
2026-09-21 21:45:39 +0530

    Update SiteHeader.tsx

--- src/components/site/SiteHeader.tsx
+++ src/components/site/SiteHeader.tsx
@@ -19,7 +19,7 @@ export function SiteHeader() {
       <div className="mx-auto flex max-w-6xl items-center justify-between gap-4 px-4 py-3">
         <Link to="/" className="flex items-center gap-2">
           <img
-            src={logoAsset.url}
+            src={logo}
             alt="Zuber Shaikh Electrician"
             width={520}
             height={160}
