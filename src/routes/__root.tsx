a3b6cdfa68d2561a896b090a1885cf64c2cdb1d2
KADIR0612 <shkhakadir@gmail.com>
2026-09-24 15:52:08 +0530

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -109,6 +109,13 @@ function RootShell({ children }: { children: ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-NMXFSZQZYH"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-NMXFSZQZYH');
+</script>
       </head>
       <body>
         {children}
